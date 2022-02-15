5e7d11e4e9cd83b4a4c9156c061b5dc6208b9b8c
zongili <hulya.gk@gmail.com>
2022-02-15 16:44:19 -0500

    upload majorairports json file

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,52 +2,70 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// single line map
-// Coordinates for each point to be used in the line.
-// Let's edit our logic.js file to create a line from airport la - san franc LAX to SFO.
-// let line = [
-//   [33.9416, -118.4085],
-//   [37.6213, -122.3790]
-// ];
-// Create a polyline using the line coordinates and make the line red.
-// L.polyline(line, {
-//   color: "red"
-// }).addTo(map);
-// change the coordinates for the center of the map to somewhere between LAX and SFO by adding [36.1733, -120.1794] in the setView() method.
-// •	Change the zoom level in the setView() method to 7.
-// let map = L.map("map").setView([36.1733,-120.1794], 7);
-// 
-// for multiple lines
-// 
 // Create the map object with center at the San Francisco airport.
-// let map = L.map('map').setView([37.6213, -122.3790], 5);
-// 	// Coordinates for each point to be used in the polyline.
-//   // showing the route from LAX, SFO, SLC, and SEA:
-// let line = [
-// 	  [33.9416, -118.4085],
-// 	  [37.6213, -122.3790],
-// 	  [40.7899, -111.9791],
-// 	  [47.4502, -122.3088]];
+// The L.geoJSON()layer reverses the coordinates to plot them on the map.
+// let map = L.map('map').setView([37.5, -122.5], 10);
+// Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//       "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
 
-// Make the line yellow by editing the value for the "color" key in the polyline() function to yellow.
-	// Create a polyline using the line coordinates and make the line yellow.
-// L.polyline(line, {
-// 	   color: "yellow"
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h3>" + feature.properties.name + "</h3>" + "<hr>" + "<h2>" + feature.properties.city + ", " + feature.properties.country + "</h2>");
+//   }
+// }).addTo(map);
+// single point for GeoJSON
+// add a popup marker for each feature and add data from the properties of the JavaScript object. 
+// L.geoJSON(sanFranAirport, {
+//   onEachFeature: function(feature, layer) {
+//     console.log(layer);
+//     layer.bindPopup("<h3>" + "Airport code:, " + feature.properties.faa + "  " + feature.properties.name + "</h3>" + "<hr>" + "<h2>" + feature.properties.city + ", " + feature.properties.country + "</h2>");
+//    }
 // }).addTo(map);
 
-let map = L.map('map').setView([43.6777, -79.6248], 5);
-let line = [
-  [37.6213, -122.3790],
-  [30.1975, -97.6664],
-  [43.6777, -79.6248],
-  [40.6413, -73.7781]];
+// Map Multiple GeoJSON Points
+// geographical center of the Earth and set the zoom level as follows:
+	// Create the map object with center and zoom level.
+let map = L.map('map').setView([30, 30], 2);
+// We create the tile layer that will be the background of our map.
+// 
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
 
-  L.polyline(line,{
-    opacity: 0.5,
-    weigth: 4,
-    color: "blue",
-    dashArray:[2, 5]
-  }).addTo(map);
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
 
 // // To change the map's style, change the map id using the list of Mapbox ids below:
 
@@ -61,13 +79,3 @@ let line = [
 // streets.addTo(map);
 
 // Method 2 - Use the Mapbox Styles API
-// We create the tile layer that will be the background of our map.
-// 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
-
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
