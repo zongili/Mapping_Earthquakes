b6fd41f0ad33a68f3f6b87546b3d13386d6fe886
zongili <hulya.gk@gmail.com>
2022-02-15 18:39:59 -0500

    dark-ligth view tiles, layer control using d3json

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -46,7 +46,7 @@ console.log("working");
 // Map Multiple GeoJSON Points
 // geographical center of the Earth and set the zoom level as follows:
 	// Create the map object with center and zoom level.
-let map = L.map('map').setView([30, 30], 2);
+// let map = L.map('map').setView([30, 30], 2);
 // We create the tile layer that will be the background of our map.
 // 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -54,19 +54,43 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: API_KEY
 });
+
+// using this to add dark layer, swithing between maps
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY });
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+// Create the map object with center, zoom level and default layer.
+let map = L.map('map', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// streets.addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/zongili/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
 
-// Grabbing our GeoJSON data.
+// Grabbing our GeoJSON data and adding popup marker+info
 d3.json(airportData).then(function(data) {
   console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+  L.geoJson(data, {
+     onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3>" + "Airport code:, " + feature.properties.faa + "  " + feature.properties.name + "</h3>" + "<hr>" + "<h2>" + feature.properties.city + ", " + feature.properties.country + "</h2>");
+    }
+  }).addTo(map);
 });
-
 // // To change the map's style, change the map id using the list of Mapbox ids below:
 
 // // mapbox/streets-v11
@@ -78,4 +102,4 @@ L.geoJSON(data).addTo(map);
 // // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map);
 
-// Method 2 - Use the Mapbox Styles API
+
