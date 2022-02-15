da2ab89d17b1a0244b64cce990aa76cbe74d8364
zongili <hulya.gk@gmail.com>
2022-02-15 09:47:06 -0500

    dotted lines, multiple locations

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,54 +2,53 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-// This method is useful when we need to add multiple tile layers, or a background image of our map(s), 
-let map = L.map("map", {
-    center: [40.7, -94.5],
-    zoom: 4
-  });
-  
-  //  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// single line map
+// Coordinates for each point to be used in the line.
+// Let's edit our logic.js file to create a line from airport la - san franc LAX to SFO.
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790]
+// ];
+// Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//   color: "red"
+// }).addTo(map);
+// change the coordinates for the center of the map to somewhere between LAX and SFO by adding [36.1733, -120.1794] in the setView() method.
+// •	Change the zoom level in the setView() method to 7.
+// let map = L.map("map").setView([36.1733,-120.1794], 7);
 // 
+// for multiple lines
+// 
+// Create the map object with center at the San Francisco airport.
+// let map = L.map('map').setView([37.6213, -122.3790], 5);
+// 	// Coordinates for each point to be used in the polyline.
+//   // showing the route from LAX, SFO, SLC, and SEA:
+// let line = [
+// 	  [33.9416, -118.4085],
+// 	  [37.6213, -122.3790],
+// 	  [40.7899, -111.9791],
+// 	  [47.4502, -122.3088]];
 
-// add multiple markers
-// let map = L.map("map").setView([34.0522, -118.2487], 14);
-// An array containing each city's location, state, and population.
-// get data from cities.js
-let cityData = cities;
+// Make the line yellow by editing the value for the "color" key in the polyline() function to yellow.
+	// Create a polyline using the line coordinates and make the line yellow.
+// L.polyline(line, {
+// 	   color: "yellow"
+// }).addTo(map);
 
-// Loop through the cities array and create one marker for each city.
-// Loop through the cities array and create one marker for each city.
-// using toLocaleString to popup markers have the population formatted with a thousands separator
-// cityData.forEach(function(city) {
-//   console.log(city)
-//   L.marker(city.location)
-//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//   .addTo(map);
-// });
-// change the marker for each city to a circle that has a radius equivalent to the city's population. 
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/100000, // if don't divide circle is too large
-    color: "orange",
-    fillColor: "orange"
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+let map = L.map('map').setView([43.6777, -79.6248], 5);
+let line = [
+  [37.6213, -122.3790],
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]];
 
+  L.polyline(line,{
+    opacity: 0.5,
+    weigth: 4,
+    color: "blue",
+    dashArray:'2, 5'
+  }).addTo(map);
 
-// method 1 - Use the Leaflet Documentation
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: API_KEY
-// });
 // // To change the map's style, change the map id using the list of Mapbox ids below:
 
 // // mapbox/streets-v11
@@ -64,7 +63,7 @@ cityData.forEach(function(city) {
 // Method 2 - Use the Mapbox Styles API
 // We create the tile layer that will be the background of our map.
 // 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
